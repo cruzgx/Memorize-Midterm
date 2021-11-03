53edb68311785e98174a78bd3e36c284e622a45a
Cruz Gonzalez <cruzgonzalez888@gmail.com>
2021-11-02 22:26:19 -0400

    Created SlotItem.swift

--- Midterm/ContentView.swift
+++ Midterm/ContentView.swift
@@ -9,8 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Howdy")
-            .padding()
+            //.padding()
+        VStack {
+            ScrollView {
+                Text("Balance: $ X")
+                    .font(.largeTitle)
+                
+                HStack {
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Text("Left Button")
+                    })
+                    Spacer()
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Text("Right Button")
+                    })
+                    
+                }
+                .padding()
+            }
+        }
+        
     }
 }
 
---
+++ Midterm/SlotItem.swift
@@ -0,0 +1,27 @@
+//
+//  SlotItem.swift
+//  Midterm
+//
+//  Created by Cruz Gonzalez Garcia on 11/2/21.
+//  One of the 3 items being shown as a slot machine
+
+//  Think of this represening of the 7s in a 7 7 7 slot matchine
+import Foundation
+
+struct SlotItem {
+    //features that a SlotItem has
+    
+    
+    enum Shapes {
+        //CHECK SPELLING
+        case isometricTriangle // ▲
+        case rightTraingle
+        case downTraingel
+        case leftTraingle
+        case square
+        case circle
+        case diamond
+    
+    }
+    
+}

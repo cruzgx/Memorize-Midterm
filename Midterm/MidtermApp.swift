004c2f390940cb6fe4e7065edb360c9e848dd1ec
Cruz Gonzalez <cruzgonzalez888@gmail.com>
2021-11-02 21:23:10 -0400

    Initial Commit

---
+++ Midterm/MidtermApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidtermApp.swift
+//  Midterm
+//
+//  Created by Cruz Gonzalez Garcia on 11/2/21.
+//
+
+import SwiftUI
+
+@main
+struct MidtermApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
